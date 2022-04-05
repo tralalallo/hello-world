c8b56651004de81fa5f007f4417f8d88837cfbba
Alessandro Ventura <alessandro.ventura10@studio.unibo.it>
2022-04-05 17:57:50 +0200

    added py file

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+#this program prints "Hello World" a bunch of times:
+
+
+value = input("how many times should I say Hello World?\n")
+value = int(value)
+
+for i in range(value):
+    print(i+1,"Hello World!")
+
+
